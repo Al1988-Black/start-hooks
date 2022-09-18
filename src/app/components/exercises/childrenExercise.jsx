72b2ebf023f76e476561394ea4474827a8c24e66
Al1988-Black <alexblackskja@gmail.com>
2022-09-18 23:01:38 +0300

    task11hooks+dop

--- src/app/components/exercises/childrenExercise.jsx
+++ src/app/components/exercises/childrenExercise.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-import CollapseWrapper from "../common/collapse";
 import PropTypes from "prop-types";
+import CollapseWrapper from "../common/collapse";
+import Divider from "../common/divider";
+import SmallTitle from "../common/typografy/smallTitle";
 
-const BlockComponents = ({ children }) => {
-    return React.Children.map(children, (child, index) => {
-        const config = {
-            ...child.props,
-            id: index
-        };
-        return <li>{React.cloneElement(child, config)}</li>;
-    });
-};
-
-BlockComponents.propTypes = {
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node
-    ])
-};
 const ChildrenExercise = () => {
     return (
         <CollapseWrapper title="Упражнение">
@@ -28,19 +14,97 @@ const ChildrenExercise = () => {
                 <code>React.Children.map</code> так и{" "}
                 <code>React.Children.toArray</code>
             </p>
-            <ol>
-                <BlockComponents>
-                    <Component />
-                    <Component />
-                    <Component />
-                </BlockComponents>
-            </ol>
+            <Divider />
+            <SmallTitle>Решение</SmallTitle>
+
+            <ComponentsList>
+                <Component />
+                <Component />
+                <Component />
+            </ComponentsList>
         </CollapseWrapper>
     );
 };
-
-const Component = () => {
-    return <div>Компонент списка</div>;
+const ComponentsList = ({ children }) => {
+    const arrayOfChildren = React.Children.toArray(children);
+    console.log(arrayOfChildren);
+    return React.Children.map(arrayOfChildren, (child) =>
+        React.cloneElement(child, {
+            ...child.props,
+            num: +child.key.replace(".", "") + 1
+        })
+    );
 };
-
+ComponentsList.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
+const Component = ({ num }) => {
+    console.log(num);
+    return <div>{num} Компонент списка</div>;
+};
+Component.propTypes = {
+    num: PropTypes.oneOfType([PropTypes.number, PropTypes.string])
+};
 export default ChildrenExercise;
+
+// import React from "react";
+// import CollapseWrapper from "../common/collapse";
+// import PropTypes from "prop-types";
+
+// // const BlockComponents = ({ children }) => {
+// //     return React.Children.map(children, (child, index) => {
+// //         const config = {
+// //             ...child.props,
+// //             id: index
+// //         };
+// //         return <li>{React.cloneElement(child, config)}</li>;
+// //     });
+// // };
+
+// class BlockComponents extends React.Component {
+//     render() {
+//         const children = React.Children.toArray(this.props.children);
+//         console.log(children);
+//         return children.map((child) => {
+//             return <li key={child.key}>{child}</li>;
+//         });
+//     }
+// }
+
+// console.log(BlockComponents);
+
+// BlockComponents.propTypes = {
+//     children: PropTypes.oneOfType([
+//         PropTypes.arrayOf(PropTypes.node),
+//         PropTypes.node
+//     ])
+// };
+// const ChildrenExercise = () => {
+//     return (
+//         <CollapseWrapper title="Упражнение">
+//             <p className="mt-3">
+//                 У вас есть компоненты Списка. Вам необходимо к каждому из них
+//                 добавить порядковый номер, относительно того, как они
+//                 располагаются на странице. Вы можете использовать как{" "}
+//                 <code>React.Children.map</code> так и{" "}
+//                 <code>React.Children.toArray</code>
+//             </p>
+//             <ol>
+//                 <BlockComponents>
+//                     <Component />
+//                     <Component />
+//                     <Component />
+//                 </BlockComponents>
+//             </ol>
+//         </CollapseWrapper>
+//     );
+// };
+
+// const Component = () => {
+//     return <div>Компонент списка</div>;
+// };
+
+// export default ChildrenExercise;
--- src/app/components/exercises/useRefExercise.jsx
+++ src/app/components/exercises/useRefExercise.jsx
@@ -1,17 +1,13 @@
-import React, { useRef, useState } from "react";
+import React, { useRef } from "react";
 import CollapseWrapper from "../common/collapse";
 const UseRefExercise = () => {
-    const [state, setState] = useState(false);
-    const data = useRef({ height: 40, width: 60, content: "Блок" });
+    const blockRef = useRef();
 
-    const handleUpdate = () => {
-        setState((prevState) => !prevState);
-        data.current = state
-            ? { height: 150, width: 80, content: "Text" }
-            : { height: 40, width: 60, content: "Блок" };
-        console.log(data.current, state);
+    const handleClick = () => {
+        blockRef.current.children[0].innerText = "text";
+        blockRef.current.style.width = "150px";
+        blockRef.current.style.height = "80px";
     };
-
     return (
         <CollapseWrapper title="Упражнение">
             <p className="mt-3">
@@ -23,19 +19,63 @@ const UseRefExercise = () => {
                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
             </ul>
             <div
+                ref={blockRef}
                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
                 style={{
-                    height: data.current.height,
-                    width: data.current.width,
+                    height: 40,
+                    width: 60,
                     color: "white"
                 }}
-                onClick={handleUpdate}
-                role="button"
             >
-                <small>{data.current.content}</small>
+                <small>Блок</small>
             </div>
+            <button className="btn btn-secondary my-4" onClick={handleClick}>
+                Trigger action
+            </button>
         </CollapseWrapper>
     );
 };
 
 export default UseRefExercise;
+
+// import React, { useRef, useState } from "react";
+// import CollapseWrapper from "../common/collapse";
+// const UseRefExercise = () => {
+//     const [state, setState] = useState(false);
+//     const data = useRef({ height: 40, width: 60, content: "Блок" });
+
+//     const handleUpdate = () => {
+//         setState((prevState) => !prevState);
+//         data.current = state
+//             ? { height: 150, width: 80, content: "Text" }
+//             : { height: 40, width: 60, content: "Блок" };
+//         console.log(data.current, state);
+//     };
+
+//     return (
+//         <CollapseWrapper title="Упражнение">
+//             <p className="mt-3">
+//                 У вас есть блок, у которого заданы ширина и высота. Добавьте
+//                 кнопку, при нажатии которой изменятся следующие свойства:
+//             </p>
+//             <ul>
+//                 <li>Изменится содержимое блока на &quot;text&quot;</li>
+//                 <li>высота и ширина станут равны 150 и 80 соответственно</li>
+//             </ul>
+//             <div
+//                 className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
+//                 style={{
+//                     height: data.current.height,
+//                     width: data.current.width,
+//                     color: "white"
+//                 }}
+//                 onClick={handleUpdate}
+//                 role="button"
+//             >
+//                 <small>{data.current.content}</small>
+//             </div>
+//         </CollapseWrapper>
+//     );
+// };
+
+// export default UseRefExercise;
